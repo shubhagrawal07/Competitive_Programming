6bca9fb6c55148cc1a1465be459b68f220a960ca
Shubh Agrawal <shubhagrawal0710@gmail.com>
2023-10-02 20:49:56 +0530

    First commit

---
+++ CPP_Template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define ump unordered_map<int,int>
+#define loop(i,n) for(int i=0;i<n;i++)
+
+
+
+void solve(){
+   int n;
+   cin>>n;
+   int arr[n];
+   loop(i,n) cin>>arr[n];
+   
+   
+}
+
+int32_t main() {
+
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    solve();
+	}
+	
+	return 0;
+}
